694a7c6f45771eb2a0f35075631f9da1faf8c5aa
Your Name <kiyum1406@gmail.com>
2026-09-24 10:03:02 -0700

    completed treeSum method

--- src/Practice.java
+++ src/Practice.java
@@ -218,6 +218,33 @@ public class Practice {
      * @return true if the sums are equal, false otherwise
      */
     public static boolean sumMatch(BinaryTreeNode<Integer> root, ListNode<Integer> head) {
-        return false;
+        int treeSum = treeSum(root);
+        int listSum = listSum(head);
+
+        return treeSum == listSum;
+    }
+
+    private static int treeSum(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return root.data
+                + treeSum(root.left)
+                + treeSum(root.right);
+    }
+
+    private static int listSum(ListNode<Integer> head) {
+        int sum = 0;
+
+        ListNode<Integer> current = head;
+
+        while (current != null) {
+            sum += current.data;
+            current = current.next;
+        }
+
+        return sum;
+    
     }
 }
