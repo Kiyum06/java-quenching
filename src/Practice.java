61a4620667f73f66391e260be718df40148a983f
Your Name <kiyum1406@gmail.com>
2026-09-24 00:08:37 -0700

    completed frequencies method

--- src/Practice.java
+++ src/Practice.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -129,7 +130,23 @@ public class Practice {
      * @return a frequency map of values in the list
      */
     public static <T> Map<T, Integer> frequencies(ListNode<T> head) {
-        return null;
+        Map<T, Integer> result = new HashMap<>();
+
+        ListNode<T> current = head;
+
+        while (current != null) {
+            T value = current.data;
+
+            if (result.containsKey(value)) {
+                result.put(value, result.get(value) + 1);
+            } else {
+                result.put(value, 1);
+            }
+
+            current = current.next;
+        }
+
+        return result;
     }
 
 
